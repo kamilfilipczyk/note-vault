1af78d109fba441f6cb34c82ac80e1e55a5cf6ae
Kamil <kamil.filipczyk.kontakt@gmail.com>
2024-06-25 12:49:24 +0200

    Add comments and minor code fixes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import create_app method from our website package
 from website import create_app
 
 # assign instance of Flask application to "app" variable
@@ -5,4 +6,5 @@ app = create_app()
 
 # run the Flask application only if this script is executed directly
 if __name__ == "__main__":
+    # debug=True automatically reruns the server after changes in code
     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,11 @@
+# import Flask class from flask package
 from flask import Flask
 
 def create_app():
+    # create new instance of the Flask class and assign it to "app" variable
     app = Flask(__name__)
-    app.config['SECRET KEY'] = "nyarlathotep"
+    # set the secret key which will be used for securing data
+    app.config['SECRET_KEY'] = "nyarlathotep"
+
+    # return the configured instance of the Flask class
     return app
