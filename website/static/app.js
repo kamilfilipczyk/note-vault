a04a37f387072a99965956738e4b0dcdef5965ea
Kamil <kamil.filipczyk.kontakt@gmail.com>
2024-06-27 11:19:07 +0200

    Add top bar and fix minor bugs

--- website/static/app.js
+++ website/static/app.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sidebar = document.getElementById("sidebar");
     const hamburgerMenu = document.getElementById("hamburger-menu");
+    const homeLink = document.getElementById("home-link");
 
     hamburgerMenu.addEventListener("click", function() {
         sidebar.classList.toggle("collapsed");
         document.querySelector(".content").classList.toggle("shifted");
+    });
+
+    homeLink.addEventListener("click", function() {
+        window.location.href = "/";
     });
 });
