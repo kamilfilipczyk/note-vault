15b4a32f188a849a7eb6abf38a757b0ff4ad4286
Kamil <kamil.filipczyk.kontakt@gmail.com>
2024-06-28 14:40:47 +0200

    Add fetching the user data

--- website/auth.py
+++ website/auth.py
@@ -4,10 +4,18 @@ auth = Blueprint("auth", __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def log_in():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
     return render_template('login.html')
 
 @auth.route('/signup', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        user_name = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm-password')
     return render_template('signup.html')
 
 @auth.route('/logout')
