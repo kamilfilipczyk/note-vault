d398c993590003d80d9ba110bc67d2fb7d068ea0
Kamil <kamil.filipczyk.kontakt@gmail.com>
2024-07-02 12:01:49 +0200

    Initialize database

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,18 @@
 # import Flask class from flask package
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "note_vault.db"
 
 def create_app():
     # create new instance of the Flask class and assign it to "app" variable
     app = Flask(__name__)
     # set the secret key which will be used for securing data
     app.config['SECRET_KEY'] = "nyarlathotep"
+    # set database location
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     # in order to register our blueprints its necessary to import them first
     from .views import views
     from .auth import auth
@@ -15,4 +22,3 @@ def create_app():
 
     # return the configured instance of the Flask class
     return app
-
